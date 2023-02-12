7b1f5ea9c9e8186b1c6bc76cdc50f1fce95f1b99
LearnedDr <fredrickj.chang@gmail.com>
2023-02-12 13:09:11 -0500

    added ids

--- routes/notes.js
+++ routes/notes.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
+const uuid = require('../helpers/uuid.js')
 
 router.get('/', (req, res) =>
 readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
 );
 
-module.exports = router
 
 router.post('/', (req, res) => {
     console.log(req.body);
     const { title, text } = req.body;
+    
     if (title && text) {
         const newNotePost = {
             title,
             text,
+            id : uuid(),
         };
         readAndAppend(newNotePost, './db/notes.json');
         // console.log(newNotePost);
-
+        
         const response = {
             status: 'success',
             body:newNotePost,
         };
-
+        
+        // res.send(res.json(response));
         res.json(response);
+        
     } else {
         res.json('Error in posting new note')
     }
 });
 
+module.exports = router
 // // // -------- REQUIRED IMPORTS --------
 // const express = require("express");
 // const router = express.Router();
